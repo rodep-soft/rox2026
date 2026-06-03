2beace10969ee704284a2de2362c318a7ef159c6
ryo-0723 <tengbenliang742@gmail.com>
2026-06-03 17:25:59 +0900

    change format type

--- ros2_ws/src/ros2_practice/src/keyboard_sub copy.cpp
+++ ros2_ws/src/ros2_practice/src/keyboard_sub copy.cpp
@@ -5,25 +5,26 @@
 
 class TextSubscriber : public rclcpp::Node
 {
-  public:
-    TextSubscriber() : Node("text_subscriber")
-    {
-        auto topic_callback = [this](const std_msgs::msg::String::SharedPtr msg) -> void {
-            RCLCPP_INFO(this->get_logger(), "受信: '%s'", msg->data.c_str());
-        };
-        subscription_ = this->create_subscription<std_msgs::msg::String>("chatter", 10, topic_callback);
-    }
+public:
+  TextSubscriber()
+  : Node("text_subscriber")
+  {
+    auto topic_callback = [this](const std_msgs::msg::String::SharedPtr msg) -> void {
+        RCLCPP_INFO(this->get_logger(), "受信: '%s'", msg->data.c_str());
+      };
+    subscription_ = this->create_subscription<std_msgs::msg::String>("chatter", 10, topic_callback);
+  }
 
-  private:
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+private:
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
-int main(int argc, char* argv[])
+int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);
+  rclcpp::init(argc, argv);
 
-    rclcpp::spin(std::make_shared<TextSubscriber>());
+  rclcpp::spin(std::make_shared<TextSubscriber>());
 
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::shutdown();
+  return 0;
 }
