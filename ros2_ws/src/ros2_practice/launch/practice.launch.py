83a5a8536825e21e86ce63690bf78719c57761b7
ryo-0723 <tengbenliang742@gmail.com>
2026-06-19 02:40:44 +0900

    test formatter

--- ros2_ws/src/ros2_practice/launch/practice.launch.py
+++ ros2_ws/src/ros2_practice/launch/practice.launch.py
@@ -1,20 +1,20 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-
-def generate_launch_description():
+def  generate_launch_description():
 
     return LaunchDescription(
         [
-            Node(
+            Node
+            (
                 package="ros2_practice",
                 executable="keyboard_pub",
                 name="keyboard_publisher",
             ),
-            Node(
-                package="ros2_practice",
-                executable="keyboard_sub",
-                name="text_subscriber",
+            Node (
+                package="ros2_practice", 
+                executable="keyboard_sub" ,
+                name = "text_subscriber" ,
             ),
         ]
-    )
+     )
--- ros2_ws/src/ros2_practice/src/keyboard_sub.cpp
+++ ros2_ws/src/ros2_practice/src/keyboard_sub.cpp
@@ -5,22 +5,21 @@
 
 class TextSubscriber : public rclcpp::Node
 {
-public:
-  TextSubscriber()
-  : Node("text_subscriber")
-  {
+
+   public:TextSubscriber()
+  : Node("text_subscriber"){
     subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "chatter", 10, std::bind(&TextSubscriber::topic_callback, this, std::placeholders::_1));
+      "chatter" , 10 , std::bind(&TextSubscriber::topic_callback, this, std::placeholders::_1));
   }
 
 private:
-  void topic_callback(const std_msgs::msg::String::SharedPtr msg)const
-  {
+void  topic_callback(const std_msgs::msg::String::SharedPtr msg)const
+    {
     RCLCPP_INFO(this->get_logger(), "受信: '%s'", msg->data.c_str());
-  }
+    }
 
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-};
+}  ;
 
 int main(int argc, char * argv[])
 {
