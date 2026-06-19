ea0a474b8f8c5446d072bf9e618739d2ecba0c26
CyaNabe <abe.mitsunobu2188@mail.kyutech.jp>
2026-06-19 13:06:14 +0900

    update base_controller

--- ros2_ws/src/base_controller/src/base_controller.cpp
+++ ros2_ws/src/base_controller/src/base_controller.cpp
@@ -4,7 +4,13 @@
 
 class BaseController : public rclcpp::Node {
 public:
-  BaseController() : Node("base_controller"), max_linear_velocity_(2.0), angular_velocity_(2.0) {
+  BaseController() : Node("base_controller") {
+    this->declare_parameter("max_linear", 2.0);
+    this->declare_parameter("max_angular", 2.0);
+
+    max_linear_velocity_ = this->get_parameter("max_linear").as_double();
+    angular_velocity_ = this->get_parameter("max_angular").as_double();
+
     joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
     "joy", 10, std::bind(&BaseController::joy_callback, this, std::placeholders::_1));
 
@@ -19,9 +25,9 @@ private:
   void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg) {
   
     auto cmd_msg = geometry_msgs::msg::Twist();
-    cmd_msg.linear.x = msg->axes[0] * max_linear_velocity_;  // 前後の移動;
+    cmd_msg.linear.x = msg->axes[0] * -1 * max_linear_velocity_;  // 前後の移動;
     cmd_msg.linear.y = msg->axes[1] * max_linear_velocity_;  // 左右の移動;
-    cmd_msg.angular.z = msg->buttons[17] * angular_velocity_ - msg->buttons[16] * angular_velocity_; // yawの回転
+    cmd_msg.angular.z = msg->axes[3] * angular_velocity_; // yawの回転
     vel_pub_->publish(cmd_msg);
   }
 
