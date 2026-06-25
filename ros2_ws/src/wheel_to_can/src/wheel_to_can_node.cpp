1a4c4f23438ff10228c21265c038fbf37605f088
revolution12181 <kosahayu.1218@icloud.com>
2026-06-25 19:12:22 +0900

    モーターの設定を追加

--- ros2_ws/src/wheel_to_can/src/wheel_to_can_node.cpp
+++ ros2_ws/src/wheel_to_can/src/wheel_to_can_node.cpp
@@ -5,6 +5,8 @@
 #include <array>
 #include <cstdint>
 #include <algorithm>
+#include <chrono>
+#include <thread>
 
 class WheelToCanNode : public rclcpp::Node
 {
@@ -17,9 +19,80 @@ public:
         pub_ = this->create_publisher<can_msgs::msg::Frame>("CAN_can0_transmit", 10);
 
         RCLCPP_INFO(this->get_logger(), "WheelToCanNode started");
+
+        //少しだけ待ってから初期化を実行
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+        init_motors(); //モーターの有効化
     }
 
+    ~WheelToCanNode(){
+        RCLCPP_INFO(this->get_logger(), "Stopping all motors");
+        stop_motors(); //モーターを止める
+    }
+
+
 private:
+    void init_motors(){
+        for (int motor_id = 1; motor_id <= 4; motor_id++){
+            //6.8 Command 6: Set Operation Mode Velocity modeに設定
+            can_msgs::msg::Frame frame_mode;
+            frame_mode.id = motor_id;
+            frame_mode.is_extended = false; //11bit
+            frame_mode.dlc = 8; //データ長は 8 固定
+
+            for (int i = 0; i < 6; i++){
+                frame_mode.data[i] = 0xFF; //前半6バイトはすべて 0xFF
+            }
+
+            frame_mode.data[6] = 0x02; //Velocity mode
+            frame_mode.data[7] = 0xFC; //Command 1 の識別子
+
+            pub_->publish(std::move(frame_mode));
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+            //6.3 Command 1: モーターの有効化
+            can_msgs::msg::Frame frame_enable;
+            frame_enable.id = motor_id;
+            frame_enable.is_extended = false; // 11bit
+            frame_enable.dlc = 8;             // データ長は 8 固定
+
+            for (int i = 0; i < 7; i++){
+                frame_enable.data[i] = 0xFF;   // 前半7バイトはすべて 0xFF
+            }
+
+            frame_enable.data[7] = 0xFC;    // Command 1 の識別子
+
+            pub_->publish(std::move(frame_enable));
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+            RCLCPP_INFO(this->get_logger(), "Motor %d: Velocity Mode Set & Enabled", motor_id);
+        
+        }
+    }
+
+    void stop_motors(){
+        for (int motor_id = 1; motor_id <= 4; motor_id++) {
+            can_msgs::msg::Frame frame_stop;
+            frame_stop.id = motor_id;          // 上位3ビットは0、そのままモーターid
+            frame_stop.is_extended = false;   // 11bit 
+            frame_stop.dlc = 8;               // データ長は 8 固定
+        for (int i = 0; i < 7; i++) {
+                frame_stop.data[i] = 0xFF; //7バイトFFにする
+            }
+
+        frame_stop.data[7] = 0xFD;
+
+        pub_->publish(std::move(frame_stop));
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+        RCLCPP_INFO(this->get_logger(), "Motor %d: Sent Stop Command", motor_id);
+        }
+    }
+
     void callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
     {
         //データのサイズが4未満なら警告
@@ -43,7 +116,7 @@ private:
             std::copy(cur_bytes.begin(), cur_bytes.end(), buffer + 4);
 
             can_msgs::msg::Frame frame;
-            frame.id = 0x140 + motor_id;  // // MIT 6.13 Velocity Mode Control
+            frame.id = 0x200 + motor_id;    // MIT 6.13 Velocity Mode Control
             frame.is_extended = false;     //11bit
             frame.dlc = 8;                // データ長は 8 固定
 
