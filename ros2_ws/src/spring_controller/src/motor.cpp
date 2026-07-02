9c63339e6b54e98672c0dd7cf5d7947a30c1b597
onexy15 <kentayui95@gmail.com>
2026-07-02 17:32:40 +0900

    出したトピックを受け取って，モータの動きを計算して速度をトピックに出すノード

---
+++ ros2_ws/src/spring_controller/src/motor.cpp
@@ -0,0 +1,118 @@
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "std_msgs/msg/float32.hpp"
+#include "std_msgs/msg/u_int8_multi_array.hpp"
+#include "std_msgs/msg/u_int8.hpp"
+
+class SprMot : public rclcpp::Node
+{
+    public :
+    SprMot()
+    : Node ("spr_mot")
+    {
+        spr_sub_ = 
+        this->create_subscription<std_msgs::msg::UInt8>(
+            "/spring_cmd",
+            10,
+            std::bind(&SprMot::spring_callback,this,std::placeholders::_1);
+        )
+        
+        limit_sub_ = 
+        this -> create_subscription<std_msgs::msg::UInt8MultiArray>(
+            "/limit_sw",
+            10,
+            std::bind(&SprMot::limit_callback,this,std::placeholders::_1);
+        )
+
+        spe_pub_ = 
+        this->create_publisher<std_msgs::msg::Float32>(
+            "/edulate_speed",
+            10
+        );
+
+        timer_ = 
+        this -> create_wall_timer(
+            std::chrono::milliseconds(20);
+            std::bind(&SprMot::timer_callback,this)
+        );
+    }
+
+    private:
+    enum State{
+        LOAD, //装填中
+        FIRE, //発射中
+        STOP  //停止
+    };
+
+    State state_ = LOAD; //state_→状態を表す
+    bool fire_request_ = false; //発射命令がtrueかfalseか
+    bool limit_on_ = false; //リミットスイッチが押されているかどうか 
+    //limit_on_がtrue　→　発射準備完了
+    int fire_count_ = 0; //発射中にどれぐらい時間がたったか
+    void spring_callback(const std_msgs::msg::UInt8::SharedPtr msg)
+    {
+        if(msg -> masage == 1){
+            fire_request_ = true;
+        }
+    }
+
+    void limit_callback(const std_msgs::msg::UInt8MultiArray::SharedPtr msg){
+        if(msg->data.size() > 0)
+        {
+            limit_on_ = msg -> data[0];
+        }
+    }
+
+    void timer_callback()
+    {
+        std_msgs::msg::Float32 speed;
+
+        switch(state_){
+        case LOAD:
+        if(limit_on_){ //リミットスイッチが押されている
+            speed.data = 0.0;
+        
+        if(fire_request_){ //発射命令がある
+            fire_request_ = false;
+            fire_count_ = 0;
+            state_ = FIRE;
+        }}
+        else{ //リミットスイッチが押されていない
+            speed.data = 20.0;
+        }
+
+        break;
+
+        case FIRE:
+        speed.data = 20.0;
+        fire_count_ ++;
+
+        if(fire_count_ > 10){
+            state_ = LOAD;
+        }
+        break;
+
+        default:
+        speed.data = 0.0;
+        break;
+    }
+
+    speed_pub_ -> publish(speed);
+}
+rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr spr_sub_;
+rclcpp::Subscription<std_msgs::msg::UInt8MultiArray>::ShraedPtr limit_sub_;
+
+rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr spe_pub_;
+
+rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc,char *argv[])
+{
+    rclcpp::init(argc,argv);
+    rclcpp::spin(std::make_shared<SprMot>());
+    rclcpp::shutdown();
+    return 0;
+}
