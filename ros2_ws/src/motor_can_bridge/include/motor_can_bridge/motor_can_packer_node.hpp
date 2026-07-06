286672d2ab5fbbec4436982620554463ffd5e396
Zyodooon <shuhei2.1129@gmail.com>
2026-07-06 17:48:20 +0900

    resolve conflict

---
+++ ros2_ws/src/motor_can_bridge/include/motor_can_bridge/motor_can_packer_node.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include <can_msgs/msg/frame.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+class MotorCanPackerNode : public rclcpp::Node
+{
+public:
+  MotorCanPackerNode();
+
+private:
+  struct ChannelConfig
+  {
+    std::string name;
+    std::string topic;
+    uint8_t received_index;
+    uint8_t data_offset;
+  };
+
+  void DeclareParameters();
+  void GetParameters();
+  void SetupRosInterfaces();
+
+  void FrameCallback(const can_msgs::msg::Frame::SharedPtr msg, const ChannelConfig & channel);
+  bool HasReceivedAllEnabledChannels() const;
+  can_msgs::msg::Frame CreatePackedFrame(const rclcpp::Time & stamp) const;
+
+  std::string can_tx_topic_;
+  uint32_t can_id_;
+  bool is_extended_;
+
+  std::vector<ChannelConfig> channels_;
+  std::vector<rclcpp::Subscription<can_msgs::msg::Frame>::SharedPtr> subscriptions_;
+  rclcpp::Publisher<can_msgs::msg::Frame>::SharedPtr can_publisher_;
+
+  std::array<uint8_t, 8> frame_data_ {};
+  std::array<bool, 2> received_ {};
+};
