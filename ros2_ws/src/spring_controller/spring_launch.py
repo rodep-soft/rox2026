6c64fdb0ca92f4c6cf642be3026ca7e45ad53e76
onexy15 <kentayui95@gmail.com>
2026-07-07 18:33:28 +0900

    launchとCMakeListsの修正

--- ros2_ws/src/spring_controller/spring_launch.py
+++ ros2_ws/src/spring_controller/spring_launch.py
@@ -7,14 +7,14 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package="spring_controller",
-            executable="subscriber",
+            executable="spring_joy_sub",
             name="joy_sub",
             output="screen",
         ),
 
         Node(
             package="spring_controller",
-            executable="motor",
+            executable="spring_cal_motor_pub",
             name="spr_mot",
             output="screen",
         ),
