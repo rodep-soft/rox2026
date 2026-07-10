aa152914fd2d634e970e06be48ae0cd610700ab0
ryo-0723 <tengbnenliang742@gmail.com>
2026-07-10 17:44:55 +0900

    fix parameter and change code

--- ros2_ws/src/spring_controller/src/spring_cal_motor_pub.cpp
+++ ros2_ws/src/spring_controller/src/spring_cal_motor_pub.cpp
@@ -1,125 +1,117 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-
+#include "spring_controller/spring_controller_state.h"
 #include "std_msgs/msg/float32.hpp"
 #include "std_msgs/msg/u_int8_multi_array.hpp"
 #include "std_msgs/msg/u_int8.hpp"
 
-class SprMot : public rclcpp::Node
+class SpringMot : public rclcpp::Node
 {
-    public :
-    SprMot()
-    : Node ("spr_mot")
+public:
+    SpringMot()
+        : Node("spring_mot")
     {
-        this->declare_parameter("max_speed",20.0);
-        max_speed_ =
-            this->get_parameter("max_speed").as_double();
-        spr_sub_ = 
-        this->create_subscription<std_msgs::msg::UInt8>(
+        this->declare_parameter("max_speed", 20.0);
+        this->declare_parameter("loading_speed", 5.0);
+        this->declare_parameter("limit_sw_index", 0);
+
+        max_speed_ = this->get_parameter("max_speed").as_double();
+        loading_speed_ = this->get_parameter("loading_speed").as_double();
+        limit_sw_index_ = this->get_parameter("limit_sw_index").as_int();
+
+        spr_sub_ = this->create_subscription<std_msgs::msg::UInt8>(
             "/spring_cmd",
             10,
-            std::bind(&SprMot::spring_callback,this,std::placeholders::_1));
-        
-        limit_sub_ = 
-        this -> create_subscription<std_msgs::msg::UInt8MultiArray>(
+            std::bind(&SpringMot::spring_callback, this, std::placeholders::_1));
+
+        limit_sw_sub_ = this->create_subscription<std_msgs::msg::UInt8MultiArray>(
             "/limit_sw",
             10,
-            std::bind(&SprMot::limit_callback,this,std::placeholders::_1));
+            std::bind(&SpringMot::limit_callback, this, std::placeholders::_1));
 
-        spe_pub_ = 
-        this->create_publisher<std_msgs::msg::Float32>(
+        speed_pub_ = this->create_publisher<std_msgs::msg::Float32>(
             "/edulite_speed",
-            10
-            );
+            10);
 
-        timer_ = 
-        this -> create_wall_timer(
-            std::chrono::milliseconds(20),
-            std::bind(&SprMot::timer_callback,this));
+        // 10msごとにモーター制御管理コールバックを呼び出す
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(10),
+            std::bind(&SpringMot::timer_callback, this));
     }
 
-    private:
-    enum State{
-        LOAD, //装填中
-        FIRE //発射中
-    };
+private:
+    State state_ = State::STOP; // 制御状態を表す
+    double max_speed_;          // 発射時のモーター回転速度
+    double loading_speed_;      // 装填時のモーター回転速度
+    int limit_sw_ = 0;          // リミットスイッチの状態を表す変数
+    int limit_sw_index_;        // リミットスイッチのインデックス番号
 
-    State state_ = LOAD; //state_→状態を表す
-    double max_speed_;
-    bool fire_request_ = false; //発射命令がtrueかfalseか
-    bool limit_on_ = false; //リミットスイッチが押されているかどうか 
-    //limit_on_がtrue　→　発射準備完了
+    int fire_count_ = 0; // 発射中にどれぐらい時間がたったか
+
+    rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr spr_sub_;
+    rclcpp::Subscription<std_msgs::msg::UInt8MultiArray>::SharedPtr limit_sw_sub_;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr speed_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
     
-    int fire_count_ = 0; //発射中にどれぐらい時間がたったか
     void spring_callback(const std_msgs::msg::UInt8::SharedPtr msg)
-    {
-        if(msg -> data == 1){
-            fire_request_ = true;
-        }
+    { // 制御コマンドの受け取り
+            state_ = static_cast<State>(msg->data); // 受け取ったコマンドに応じて状態を変更
     }
-
-    void limit_callback(const std_msgs::msg::UInt8MultiArray::SharedPtr msg){
-        if(msg->data.size() > 0)
+    void limit_callback(const std_msgs::msg::UInt8MultiArray::SharedPtr msg)
+    { // リミットスイッチのデータ受け取り
+        if (msg->data.size() > 0)
         {
-            limit_on_ = msg -> data[0];
+            limit_sw_ = msg->data[limit_sw_index_];
         }
     }
 
     void timer_callback()
-    {
+    { // モーター制御管理コールバック
         std_msgs::msg::Float32 speed;
 
-        
-
-        switch(state_){
-        case LOAD:
-            if(limit_on_){ //リミットスイッチが押されている
+        switch (state_)
+        {
+        case STOP:
+            speed.data = 0.0;
+            break;
+        case LOAD://チャタリングや通り過ぎの制御も
+            if (limit_sw_ == 1)
+            { // リミットスイッチが押されている
                 speed.data = 0.0;
-        
-            if(fire_request_){ //発射命令がある
-                fire_request_ = false;
-                fire_count_ = 0;
-                state_ = FIRE;
+                if (state_ == FIRE)
+                {                    // 発射命令がある
+                    state_ = LOAD;   // 装填状態に戻す
+                    fire_count_ = 0; // 発射カウントをリセット
+                }
             }
+            else
+            { // リミットスイッチが押されていない
+                speed.data = loading_speed_;
             }
-        else{ //リミットスイッチが押されていない
-            speed.data = max_speed_;
-        }
-
-        break;
-
+            break;
         case FIRE:
             speed.data = max_speed_;
-            fire_count_ ++;
+            fire_count_++;
 
-            if(fire_count_ > 10){
+            if (fire_count_ > 10)
+            {
                 state_ = LOAD;
-        }
-        break;
-
+            }
+            break;
         default:
             speed.data = 0.0;
             break;
+        }
+
+        speed_pub_->publish(speed);
     }
-    
-
-    spe_pub_ -> publish(speed);
-}
-    rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr spr_sub_;
-    rclcpp::Subscription<std_msgs::msg::UInt8MultiArray>::SharedPtr limit_sub_;
-
-    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr spe_pub_;
-
-    rclcpp::TimerBase::SharedPtr timer_;
-
-
 };
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-    rclcpp::init(argc,argv);
-    rclcpp::spin(std::make_shared<SprMot>());
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<SpringMot>());
     rclcpp::shutdown();
     return 0;
 }
--- ros2_ws/src/spring_controller/src/spring_joy_sub.cpp
+++ ros2_ws/src/spring_controller/src/spring_joy_sub.cpp
@@ -1,63 +1,89 @@
 #include <memory>
+#include <map>
+#include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "spring_controller/spring_controller_state.h"
 #include "sensor_msgs/msg/joy.hpp"
 #include "std_msgs/msg/u_int8.hpp"
 
-class JoySub : public rclcpp::Node
+class SpringJoySub : public rclcpp::Node
 {
-    public:
-    JoySub()
-    : Node("joy_sub")
+public:
+    SpringJoySub()
+        : Node("spring_joy")
     {
-        this->declare_parameter("axis_fire",5);
-        this->declare_parameter("button_fire",10);
-        axis_fire_ = 
-            this->get_parameter("axis_fire").as_int();
-        button_fire_ =
-            this->get_parameter("button_fire").as_int();
-        subscription_ =
-        this->create_subscription<sensor_msgs::msg::Joy>(
+        // デフォルト値設定
+        this->declare_parameter("shoot_key.type", "button");
+        this->declare_parameter("shoot_key.index", 5);
+        this->declare_parameter("lock_key.type", "axis");
+        this->declare_parameter("lock_key.index", 4);
+
+        // パラメータ取得
+        joy_inputs["shoot_key"].type = this->get_parameter("shoot_key.type").as_string();
+        joy_inputs["shoot_key"].index = this->get_parameter("shoot_key.index").as_int();
+        joy_inputs["lock_key"].type = this->get_parameter("lock_key.type").as_string();
+        joy_inputs["lock_key"].index = this->get_parameter("lock_key.index").as_int();
+
+        joy_subscription = this->create_subscription<sensor_msgs::msg::Joy>(
             "joy",
             10,
-            std::bind(&JoySub::topic_callback,this,std::placeholders::_1));
-        
-        publisher_ =
-        this->create_publisher<std_msgs::msg::UInt8>(
+            std::bind(&SpringJoySub::topic_callback, this, std::placeholders::_1));
+
+        spring_publisher = this->create_publisher<std_msgs::msg::UInt8>(
             "spring_cmd",
-            10);    
+            10);
     }
 
-    private:
+private:
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_subscription;
+    rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr spring_publisher;
+
+    struct JoyInput
+    {
+        std::string type;
+        std::size_t index;
+    };
+    std::map<std::string, JoyInput> joy_inputs = {
+        {"shoot_key", {"button", 5}},
+        {"lock_key", {"axis", 4}}};
+
     void topic_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
     {
-        std_msgs::msg::UInt8 command;
-        
-        if(msg -> axes[axis_fire_] > 0.8 && msg -> buttons[button_fire_] == 1)
+        std_msgs::msg::UInt8 fire_command;
+
+        if (getValue(*msg, joy_inputs["lock_key"]) > 0.8 && getValue(*msg, joy_inputs["shoot_key"]) > 0.8)
         {
-            command.data = 1;
+            fire_command.data = static_cast<uint8_t>(State::FIRE); // 発射命令
         }
         else
         {
-            command.data = 0;
+            fire_command.data = static_cast<uint8_t>(State::LOAD); // 装填命令
         }
+        //非常停止の命令をここに書く
+        //fire_command.data = static_cast<uint8_t>(State::STOP); // 停止命令
 
-
-        publisher_->publish( command );
+        spring_publisher->publish(fire_command);
     }
-    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscription_;
-    rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr publisher_;
-    int axis_fire_;
-    int button_fire_;
 
+    float getValue(const sensor_msgs::msg::Joy &joy, const JoyInput &input)
+    { // axisとbuttonの値を取得する関数
+        if (input.type == "axis" && input.index < joy.axes.size())
+        {
+            return joy.axes[input.index];
+        }
+        else if (input.type == "button" && input.index < joy.buttons.size())
+        {
+            return joy.buttons[input.index];
+        }
+        return 0.0;
+    }
 };
 
-int main(int argc,char * argv[]){
-    rclcpp::init(argc,argv);
-
-    rclcpp::spin(std::make_shared<JoySub>());
-
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<SpringJoySub>());
     rclcpp::shutdown();
     return 0;
 }
-
