776892ab41231e87b6ab3020a4180183bffc911e
iitere <keita_iimori@icloud.com>
2026-07-14 17:47:32 +0900

    launchファイルを追加しました

--- ros2_ws/src/receive_stm32/src/receive_stm32_node.cpp
+++ ros2_ws/src/receive_stm32/src/receive_stm32_node.cpp
@@ -10,15 +10,12 @@ class LimitSwNode : public rclcpp::Node
 public:
     LimitSwNode() : Node("limit_sw_node")
     {
-        // can_sub_ のアンダーバーを追加、canCallback の大文字小文字を修正
         can_sub_ = this->create_subscription<can_msgs::msg::Frame>(
             "/CAN/can0/receive", 10,
             std::bind(&LimitSwNode::canCallback, this, std::placeholders::_1));
             
-        // UIit8MultiArray -> UInt8MultiArray に修正
         limit_sw_pub_ = this->create_publisher<std_msgs::msg::UInt8>("/limit_sw", 10);
 
-        // ログ出力のドット(.)をカンマ(,)に修正、末尾にセミコロン(;)を追加
         RCLCPP_INFO(this->get_logger(), "Limit Switch Node has been started.");
     }
 
@@ -29,11 +26,9 @@ private:
             uint8_t raw_data = msg->data[0];
             uint8_t limit_status = raw_data & 0x07;
 
-            // 送信用メッセージの作成
             auto output_msg = std_msgs::msg::UInt8();
             output_msg.data = msg->data[0];
 
-            // トピックへパブリッシュ
             limit_sw_pub_->publish(output_msg);
             
             RCLCPP_INFO(this->get_logger(), "Published limit switch state: %d", limit_status);
