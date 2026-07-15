d07be1a888d2b62df48f87f497fb1ef6e86f913a
CyaNabe <abe.Mitsunobu2188@mail.kyutech.jp>
2026-07-15 14:02:36 +0800

    fix yaml

--- ros2_ws/src/robot_bringup/launch/interface.launch.py
+++ ros2_ws/src/robot_bringup/launch/interface.launch.py
@@ -3,7 +3,7 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from ament_index_python.packages import get_package_share_directory
-import os
+
 
 
 def generate_launch_description():
