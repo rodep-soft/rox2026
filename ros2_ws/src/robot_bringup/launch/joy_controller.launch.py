8755dea5ff1b96370407a270b4e7cf42e15f89da
Zyodooon <shuhei2.1129@gmail.com>
2026-07-17 08:27:34 +0900

    joy_controllerの設定と操作仕様を追加

---
+++ ros2_ws/src/robot_bringup/launch/joy_controller.launch.py
@@ -0,0 +1,20 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    parameter_file = os.path.join(
+        get_package_share_directory('robot_bringup'), 'config', 'joy.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='joy_controller',
+            executable='joy_controller_node',
+            name='joy_controller',
+            output='screen',
+            parameters=[parameter_file],
+        ),
+    ])
