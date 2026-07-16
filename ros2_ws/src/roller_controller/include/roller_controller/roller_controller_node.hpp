1ce766071ad3c7d315b7088e9ee1118665020ef8
Zyodooon <shuhei2.1129@gmail.com>
2026-07-16 16:40:27 +0900

    roller_controllerディレクトリを復元
    main取り込み(e16e7b6)で誤って削除されていたため、削除直前の状態に復元

---
+++ ros2_ws/src/roller_controller/include/roller_controller/roller_controller_node.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/joy.hpp>
+#include <std_msgs/msg/int16.hpp>
+
+class RollerControllerNode : public rclcpp::Node
+{
+public:
+  RollerControllerNode();
+
+private:
+  void DeclareParameters();
+  void GetParameters();
+  void JoyCallback(const sensor_msgs::msg::Joy::SharedPtr msg);
+  int16_t SelectRpm(const sensor_msgs::msg::Joy & msg);
+
+  std::string rpm_topic_;
+  int enable_axis_;
+  double enable_axis_threshold_;
+  int stop_button_;
+  int high_button_;
+  int low_button_;
+  int16_t stop_rpm_;
+  int16_t high_rpm_;
+  int16_t low_rpm_;
+
+  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_subscription_;
+  rclcpp::Publisher<std_msgs::msg::Int16>::SharedPtr rpm_publisher_;
+};
