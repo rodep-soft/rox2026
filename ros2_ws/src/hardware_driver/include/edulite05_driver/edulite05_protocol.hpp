da37f1bfc19074ca728fce406118f0a1b96521d7
CyaNabe <abe.mitsunobu2188@mail.kyutech.jp>
2026-07-22 15:48:06 +0900

    保存

---
+++ ros2_ws/src/hardware_driver/include/edulite05_driver/edulite05_protocol.hpp
@@ -0,0 +1,78 @@
+//#pragma once
+
+#include <stdint.h>
+#include <array>
+#include <vector>
+#include <memory>
+#include <string>
+
+struct Canframe
+{
+    uint32_t id;
+    int dlc;  
+    uint8_t data[8];
+};
+
+class Ed05CanframeCreater
+{
+public:
+    Ed05CanframeCreater(uint8_t motor_id);
+    virtual ~Ed05CanframeCreater() = default;
+
+    virtual std::vector<Canframe> create_init_frame() = 0;
+    virtual Canframe create_control_frame(float value) = 0;
+
+protected:
+    struct ControlTargetInfo  
+    {
+        char name[12];
+        uint16_t index; // commtype18で使用
+        float value;
+    };
+
+    uint8_t motor_id_;
+    int dlc_ = 8;
+    uint8_t host_id_ = 0xFD;
+
+    uint32_t encode_can_id(uint8_t commtype_index);
+    std::array<uint8_t, 8> encode_commtype18_data(ControlTargetInfo target_info);
+
+    Canframe set_runmode(int value);
+    Canframe set_enable();
+    Canframe set_target_value(ControlTargetInfo target_info);
+    Canframe set_disable();
+    Canframe set_mechanicalzero();
+};
+
+class Velocity : public Ed05CanframeCreater
+{
+public:
+    explicit Velocity(uint8_t motor_id);
+    std::vector<Canframe> create_init_frame() override;
+    Canframe create_control_frame(float value) override;
+
+private:
+    std::array<ControlTargetInfo, 3> targets_info = {{
+        {"vel", 0x700A, 0.0f},
+        {"acc", 0x7022, 100.0f},
+        {"limit_cur", 0x7018, 5.0f},
+    }};
+};
+
+class Position : public Ed05CanframeCreater
+{
+public:
+    explicit Position(uint8_t motor_id);
+    std::vector<Canframe> create_init_frame() override;
+    Canframe create_control_frame(float value) override;
+
+private:
+    std::array<ControlTargetInfo, 5> targets_info = {{
+        {"loc_ref", 0x7016, 0.0f},
+        {"limit_cur", 0x7018, 5.0f},
+        {"vel_max", 0x7024, 50.0f},
+        {"acc_set", 0x7025, 100.0f},
+        {"zero_sta", 0x7029, 1.0f},
+    }};
+};
+
--- ros2_ws/src/hardware_driver/src/nodes/edulite05_node.cpp
+++ ros2_ws/src/hardware_driver/src/nodes/edulite05_node.cpp
@@ -1,24 +1,17 @@
 #include "rclcpp/rclcpp.hpp"
+#include "can_msgs/msg/frame.hpp"
 #include "std_msgs/msg/float32_multi_array.hpp"
-#include "edulite05_protcol.hpp"
+#include <array>
+#include <vector>
+#include "edulite05_protocol.hpp"
 
-void instance_motors()
-{
-    //インスタンス化
-    std::array<std::unique_ptr<Ed05CanframeCreater>, 6> motors;
-    motors[0] = std::make_unique<Velocity>(0x1);
-    motors[1] = std::make_unique<Velocity>(0x2);
-    motors[2] = std::make_unique<Velocity>(0x3);
-    motors[3] = std::make_unique<Velocity>(0x4);
-    motors[4] = std::make_unique<Velocity>(0x0A);
-    motors[5] = std::make_unique<Position>(0x38);
-}
 
 class Ed05DriverNode : public rclcpp::Node
 {
     
     public:
-        Ed05Driverger() {
+        Ed05DriverNode() 
+        : Node("edulite05_driver_node"){
             RCLCPP_INFO(this->get_logger(), "Edulite05Node has been started.");
 
             declare_parameters();
@@ -26,7 +19,7 @@ class Ed05DriverNode : public rclcpp::Node
 
             cmd_sub_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
             sub_topic_name_, 1, std::bind(&Ed05DriverNode::cmd_callback, this, std::placeholders::_1));
-            frame_pub_ = this->create_publisher<can_msgs::msg::Frame>(pub_topic_name, 10)//-----????-----
+            frame_pub_ = this->create_publisher<can_msgs::msg::Frame>(pub_topic_name_, 10);
 
             init_motors();
         }
@@ -46,25 +39,33 @@ class Ed05DriverNode : public rclcpp::Node
 
         rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr cmd_sub_;  
         rclcpp::Publisher<can_msgs::msg::Frame>::SharedPtr frame_pub_;  // Publisher for command messages
+        std::array<std::unique_ptr<Ed05CanframeCreater>, 6> motors_;
 
         void init_motors()
         {
+                motors_[0] = std::make_unique<Velocity>(0x1);
+                motors_[1] = std::make_unique<Velocity>(0x2);
+                motors_[2] = std::make_unique<Velocity>(0x3);
+                motors_[3] = std::make_unique<Velocity>(0x4);
+                motors_[4] = std::make_unique<Velocity>(0x0A);
+                motors_[5] = std::make_unique<Position>(0x38);
             std::vector<Canframe> frames; 
                 frame_.is_extended = true;
                 frame_.is_rtr = false;
                 frame_.is_error = false;
             
-            for (int i = 0; i < motors.size(); i++) {
-                frames = motors[i].create_init_frame();
+            for (int i = 0; i < motors_.size(); i++) {
+                frames = motors_[i]->create_init_frame();
                 for (size_t j = 0; j < frames.size(); j++) {
                     frame_.id = frames[j].id;
                     frame_.dlc = frames[j].dlc;
                     frame_.data = frames[j].data;
-                    frame_pub_->pulish(frame_);
+                    frame_pub_->publish(frame_);
                 }
             }
         }
-        cmd_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
+
+        void cmd_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
         {
             float value;
             Canframe frame;
@@ -73,26 +74,26 @@ class Ed05DriverNode : public rclcpp::Node
                 frame_.is_error = false;
 
 
-            for (int i = 0; i < motors.size(); i++)
+            for (int i = 0; i < motors_.size(); i++)
             {
                 value = *msg[i];
-                frame = motors[i].create_control_frame(value);
+                frame = motors_[i]->create_control_frame(value);
                 frame_.id = frame.id;
                 frame_.dlc = frame.dlc;
                 frame_.data = frame.data;
 
                 RCLCPP_DEBUG(this->get_logger(), "Received command[%d]: %f", i, msg->data[i]);
 
-                frame_pub_->pulish(frame_);
+                frame_pub_->publish(frame_);
             }
         }
 
-        declare_parameters()
+        void declare_parameters()
         {
             this->declare_parameter<std::string>("sub_topic_name", "cmd");
             //this->declare_parameter<uint8_t>("motor_id", 0);
         }
-        get_parameters()
+        void get_parameters()
         {
             sub_topic_name_ = this->get_parameter("sub_topic_name").as_string();
         }
