5677adbc912b58f99337b3afad91d3aefc543daf
Zyodooon <shuhei2.1129@gmail.com>
2026-08-01 15:29:54 +0900

    Launch SocketCAN lifecycle nodes directly

--- ros2_ws/src/robot_bringup/launch/ros2_socketcan.launch.py
+++ ros2_ws/src/robot_bringup/launch/ros2_socketcan.launch.py
@@ -1,45 +1,87 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+from launch.events import matches_action
+from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch_ros.actions import LifecycleNode
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
+from lifecycle_msgs.msg import Transition
+
+
+def lifecycle_handlers(node):
+    configure = RegisterEventHandler(
+        OnProcessStart(
+            target_action=node,
+            on_start=[
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=matches_action(node),
+                        transition_id=Transition.TRANSITION_CONFIGURE,
+                    )
+                )
+            ],
+        )
+    )
+    activate = RegisterEventHandler(
+        OnStateTransition(
+            target_lifecycle_node=node,
+            start_state="configuring",
+            goal_state="inactive",
+            entities=[
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=matches_action(node),
+                        transition_id=Transition.TRANSITION_ACTIVATE,
+                    )
+                )
+            ],
+        )
+    )
+    return [configure, activate]
 
 
 def generate_launch_description():
-    socketcan_launch_directory = os.path.join(
-        get_package_share_directory("ros2_socketcan"),
-        "launch",
+    receiver = LifecycleNode(
+        package="ros2_socketcan",
+        executable="socket_can_receiver_node_exe",
+        name="socket_can_receiver",
+        namespace=TextSubstitution(text=""),
+        parameters=[
+            {
+                "interface": LaunchConfiguration("can_interface"),
+                "enable_can_fd": False,
+                "interval_sec": 0.1,
+                "filters": "0:0",
+                "use_bus_time": False,
+            }
+        ],
+        remappings=[("from_can_bus", "/socketcan_bridge/rx")],
+        output="screen",
     )
-
-    receiver_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(socketcan_launch_directory, "socket_can_receiver.launch.py")
-        ),
-        launch_arguments={
-            "interface": LaunchConfiguration("can_interface"),
-            "enable_can_fd": "false",
-            "interval_sec": "0.1",
-            "from_can_bus_topic": "/socketcan_bridge/rx",
-        }.items(),
-    )
-    sender_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(socketcan_launch_directory, "socket_can_sender.launch.py")
-        ),
-        launch_arguments={
-            "interface": LaunchConfiguration("can_interface"),
-            "enable_can_fd": "false",
-            "timeout_sec": "0.01",
-            "to_can_bus_topic": "/socketcan_bridge/tx",
-        }.items(),
+    sender = LifecycleNode(
+        package="ros2_socketcan",
+        executable="socket_can_sender_node_exe",
+        name="socket_can_sender",
+        namespace=TextSubstitution(text=""),
+        parameters=[
+            {
+                "interface": LaunchConfiguration("can_interface"),
+                "enable_can_fd": False,
+                "enable_frame_loopback": False,
+                "timeout_sec": 0.01,
+            }
+        ],
+        remappings=[("to_can_bus", "/socketcan_bridge/tx")],
+        output="screen",
     )
 
     return LaunchDescription(
         [
             DeclareLaunchArgument("can_interface", default_value="can0"),
-            receiver_launch,
-            sender_launch,
+            receiver,
+            sender,
+            *lifecycle_handlers(receiver),
+            *lifecycle_handlers(sender),
         ]
     )
