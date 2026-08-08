248f9d1e96e66691af59afbff11db3b4f9fcf20a
ryo-0723 <tengbenliang742@gmail.com>
2026-08-09 02:15:25 +0900

    joyのデバイス指定の方法を変更

--- ros2_ws/src/robot_bringup/launch/input/joy.launch.py
+++ ros2_ws/src/robot_bringup/launch/input/joy.launch.py
@@ -2,18 +2,26 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
+    joy_device_id = LaunchConfiguration("joy_device_id")
+
     return LaunchDescription(
         [
+            DeclareLaunchArgument(
+                "joy_device_id",
+                default_value="0",
+                description="Joystick device index used by joy_node",
+            ),
             Node(
                 package="joy",
                 executable="joy_node",
                 name="joy_node",
                 output="screen",
                 parameters=[{
-                    'device_name': 'PS5 Controller',
+                    "device_id": ParameterValue(joy_device_id, value_type=int),
                     "autorepeat_rate": 100.0,
                     "coalesce_interval_ms": 10
                 }],
