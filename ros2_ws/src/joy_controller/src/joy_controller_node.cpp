311be13bdc80c27f483cf1924ace767a31ced26b
Zyodooon <shuhei2.1129@gmail.com>
2026-08-26 13:21:48 +0900

    アーム手動姿勢をHOMEへ変更

--- ros2_ws/src/joy_controller/src/joy_controller_node.cpp
+++ ros2_ws/src/joy_controller/src/joy_controller_node.cpp
@@ -313,7 +313,7 @@ void JoyControllerNode::loop_callback()
         joy_msg_, dpad_horizontal_axis_,
         false))                          // DPAD 右 (-1.0)
     {
-      toggle_manual_arm_position(robot_msgs::msg::ArmPosition::ZERO_POSITION);
+      toggle_manual_arm_position(robot_msgs::msg::ArmPosition::HOME);
     } else if (is_axis_just_triggered(
         joy_msg_, dpad_horizontal_axis_,
         true))                                 // DPAD 左 (+1.0)
@@ -603,7 +603,7 @@ void JoyControllerNode::toggle_manual_arm_position(uint8_t target_position)
     manual_arm_position_ == robot_msgs::msg::ArmPosition::DRIBBLE ?
     "DRIBBLE" :
     (manual_arm_position_ == robot_msgs::msg::ArmPosition::OPEN ?
-    "OPEN" : "ZERO_POSITION"));
+    "OPEN" : "HOME"));
 }
 
 void JoyControllerNode::publish_dribble_enabled(bool enabled)
