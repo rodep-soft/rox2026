0376a5cc3e9a94a076fc54861a0f1ae02c380495
ryo-0723 <tengbenliang742@gmail.com>
2026-08-27 08:35:58 +0800

    パラメータの変更

--- ros2_ws/src/robot_bringup/launch/kokura.launch.py
+++ ros2_ws/src/robot_bringup/launch/kokura.launch.py
@@ -39,7 +39,7 @@ def generate_launch_description():
             ),
             DeclareLaunchArgument(
                 "enable_vision",
-                default_value="false",
+                default_value="true",
                 description="Enable 230AI MIPI stereo vision launch (mipi_cam / hobot_stereonet / apriltag / yolo)",
             ),
             DeclareLaunchArgument(
@@ -64,7 +64,7 @@ def generate_launch_description():
             ),
             DeclareLaunchArgument(
                 "enable_apriltag",
-                default_value="false",
+                default_value="true",
                 description="Enable AprilTag detection node on CSI camera",
             ),
             DeclareLaunchArgument(
